05e2df2d290e7225fa200bb451ebdcda740724c4
nanli <linanenv@gmail.com>
2024-01-06 14:41:23 +0800

    add func_call

---
+++ src/tools/common/function_calling.py
@@ -0,0 +1,19 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+
+
+def function_calling(
+    function_desc: str, function_para: dict, prompt: ChatPromptTemplate, model_name: str
+):
+    function = {
+        "name": "function_calling",
+        "description": function_desc,
+        "parameters": function_para,
+    }
+    model = ChatOpenAI(model=model_name, temperature=0).bind(
+        function_call={"name": "function_calling"}, functions=[function]
+    )
+    runnable = {"input": RunnablePassthrough()} | prompt | model
+
+    return runnable
