5540e3ef1f3a9778e56e944f39e1f77e33e8379e
nanli <linanenv@gmail.com>
2024-04-13 10:47:59 +0000

    port to 7778

--- src/main.py
+++ src/main.py
@@ -98,4 +98,4 @@ app.mount("/oauth", oauth_app)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=7778)
