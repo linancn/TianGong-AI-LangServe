f7af0fdb7eb2a9e930aab3d2c9a851d878d913fb
nanli <linanenv@gmail.com>
2024-04-17 17:54:35 +0800

    fix .secrets/secrets.toml path

--- src/config/config.py
+++ src/config/config.py
@@ -2,7 +2,7 @@ import os
 
 import toml
 
-config = toml.load(".secrects/secrets.toml")
+config = toml.load(".secrets/secrets.toml")
 
 OPENAI_API_KEY = config["OPENAI"]["API_KEY"]
 OPENAI_MODEL = config["OPENAI"]["MODEL"]
